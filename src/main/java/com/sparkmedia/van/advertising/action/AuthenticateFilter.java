db4e59e0fd3a98a3283e85d3d595fa21ee0d8a17
Liu Hao <liuhao.htp@gmail.com>
2013-01-23 21:31:05 +0800

    add authenticateFilter logic.

--- src/main/java/com/sparkmedia/van/advertising/action/AuthenticateFilter.java
+++ src/main/java/com/sparkmedia/van/advertising/action/AuthenticateFilter.java
@@ -4,7 +4,10 @@ import sun.misc.BASE64Decoder;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import sun.misc.BASE64Decoder;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,17 +17,14 @@ import java.io.IOException;
  * A filter for user authentication.
  */
 public class AuthenticateFilter implements Filter {
-    private ServletContext context;
-    private String filterName;
-    protected FilterConfig config;
-    private int startTime, endTime;
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws ServletException, IOException {
-        HttpServletRequest req = (HttpServletRequest)request;
+            throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
         String authorization = req.getHeader("Authorization");
         if (authorization == null) {
-            askForPassword(response);
+            askForPassword(res);
         } else {
             // Authorization headers looks like "Basic blahblah",
             // where blahblah is the base64 encoded username and
@@ -35,33 +35,66 @@ public class AuthenticateFilter implements Filter {
             // Decoded part looks like "username:password".
             int index = nameAndPassword.indexOf(":");
             String user = nameAndPassword.substring(0, index);
-            String password = nameAndPassword.substring(index+1);
+            String password = nameAndPassword.substring(index + 1);
             // High security: username must be reverse of password.
             if (areEqualReversed(user, password)) {
-                showStock(request, response);
+                showStock(req, res);
             } else {
-                askForPassword(response);
+                askForPassword(res);
             }
         }
-        chain.doFilter(request,response);
+        chain.doFilter(request, response);
     }
 
-    public void init(FilterConfig config) throws ServletException {
-        this.config = config; // In case it is needed by subclass.
-        context = config.getServletContext();
-        filterName = config.getFilterName();
-        try {
-            startTime =
-                    Integer.parseInt(config.getInitParameter("startTime"));
-            endTime =
-                    Integer.parseInt(config.getInitParameter("endTime"));
-        } catch(NumberFormatException nfe) { // Malformed or null
-        // Default: access at or after 10 p.m. but before 6 a.m.
-        // is considered unusual.
-            startTime = 22; // 10:00 p.m.
-            endTime = 6; // 6:00 a.m.
-        }
+    // If no Authorization header was supplied in the request.
+    private void askForPassword(HttpServletResponse response) {
+        // Send HTTP 401
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("WWW-Authenticate",
+                "BASIC realm=\"Spark Advertising\"");
     }
+
+    // Returns true if s1 is the reverse of s2.
+    // Empty strings don't count.
+    private boolean areEqualReversed(String s1, String s2) {
+        s2 = (new StringBuffer(s2)).reverse().toString();
+        return ((s1.length() > 0) && s1.equals(s2));
+    }
+
+    // Show a Web page giving the symbol of the next hot stock.
+    private void showStock(HttpServletRequest request,
+                           HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String docType =
+                "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+                        "Transitional//EN\">\n";
+        out.println(docType +
+                "<HTML>\n" +
+                "<HEAD><TITLE>Hot Stock Tip!</TITLE></HEAD>\n" +
+                "<BODY BGCOLOR=\"#FDF5E6\">\n" +
+                "<H1>Today's Hot Stock:");
+        for (int i = 0; i < 3; i++) {
+            out.print(randomLetter());
+        }
+        out.println("</H1>\n" +
+                "</BODY></HTML>");
+    }
+
+    private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // Returns a random number from 0 to n-1 inclusive.
+    private int randomInt(int n) {
+        return ((int) (Math.random() * n));
+    }
+
+    // A random letter from the alphabet.
+    private char randomLetter() {
+        return (ALPHABET.charAt(randomInt(ALPHABET.length())));
+    }
+
+    public void init(FilterConfig config) throws ServletException {}
 
     public void destroy() {}
 }
